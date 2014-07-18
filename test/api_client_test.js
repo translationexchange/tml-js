1940202ccd748927a79588c74ddac75bdf6b6f37
Ian McDaniel <ianmcdaniel@gmail.com>
2014-07-18 16:47:44 -0700

    fixed more conflicts

--- test/api_client_test.js
+++ test/api_client_test.js
@@ -1,10 +1,12 @@
-var Tr8n = require("../lib/tr8n");
+var ApiClient = require("../lib/api_client.js");
+var Application = require("../lib/application.js");
+
 var helper = require("./test_helper");
 var assert = require("assert");
 
-describe('Tr8n.ApiClient', function() {
+describe('ApiClient', function() {
   describe('access_token', function () {
-    var application = new Tr8n.Application({
+    var application = new Application({
       key: "default",
       secret: "91ec952694f17cf3d",
       host: "http://localhost:3001"
