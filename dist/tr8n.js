7c2c3ec8ee2746b651e35f0cb806ec53bce9c375
Michael Berkovich <theiceberk@gmail.com>
2014-07-24 10:41:29 -0700

    fixed utils and config

--- dist/tr8n.js
+++ dist/tr8n.js
@@ -1208,10 +1208,10 @@ module.exports = HTMLDecorator;
 window.Tr8n = require('../tr8n');
 
 window.tr = function(label, description, tokens, options) {
-  options = utils.extend({}, options, {
-    current_locale: config.current_locale,
-    current_source: config.current_source,
-    current_translator: config.current_translator
+  options = Tr8n.utils.extend({}, options, {
+    current_locale: Tr8n.config.current_locale,
+    current_source: Tr8n.config.current_source,
+    current_translator: Tr8n.config.current_translator
   });
   return Tr8n.translate(label, description, tokens, options);
 };
