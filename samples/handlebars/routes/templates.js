ddc6eec12ae63c671d0fee60c1c59a136ede1815
Michael Berkovich <theiceberk@gmail.com>
2014-08-05 18:11:06 -0700

    added handlebars example

---
+++ samples/handlebars/routes/templates.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+router.get('/*.jst', function(req, res) {
+  var parts = req.url.split("/");
+  var file = parts[parts.length-1];
+  fs.readFile(__dirname + "/../templates/" + file, "utf-8", function(err, data) {
+    res.end(data);
+  });
+});
+
+module.exports = router;
