ec14767d45199c2a241411e0ad28041953ad3b13
Michael Berkovich <michael@translationexchange.com>
2016-01-29 19:36:12 -0800

    No more talking to the API without a CDN release

---
+++ config/application.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "default_locale":"en",
+  "languages":[
+    {
+      "locale":"en",
+      "name":"English",
+      "english_name":"English",
+      "native_name":"English",
+      "flag_url":"https://s3-us-west-1.amazonaws.com/trex-snapshots/flags/default/languages/16/en.png"
+    }
+  ],
+  "threshold":1,
+  "css":".tml_not_translated { border-bottom: 1px dotted red; } .tml_translated { border-bottom: 1px dotted green; } .tml_fallback { border-bottom: 1px dotted #e90; } .tml_pending { border-bottom: 1px dotted #e90; } .tml_locked { border-bottom: 1px dotted blue; } .tml_language_case { padding:0px 2px; border: 1px dotted blue; border-radius: 2px; } .tml_token { background: #eee; padding:0px 2px; border: 1px dotted #ccc; border-radius: 2px; color: black; }"
+};
